0cb995c2395179ec8b23c833efac82c7e0a19769
shihab-shamim <shihabshamim767@gmail.com>
2025-02-19 14:46:10 +0600

    80 persent woek done

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n'), 'version' => '080e6f3220811f18a27d');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n'), 'version' => 'ce8f9d96e7c238d7c00e');
