f21234c1500d16dd9ad0d9bb9b8a2fb62cbc5739
shihab-shamim <shihabshamim767@gmail.com>
2025-02-19 15:33:31 +0600

    start opton section

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n'), 'version' => 'ce8f9d96e7c238d7c00e');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n'), 'version' => '5a4cd025585508a5f243');
